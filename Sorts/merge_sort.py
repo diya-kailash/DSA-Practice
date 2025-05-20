fd58d10fb9065cb028ea96def8d0835d167dc37a
diya-kailash <diyakailash04@gmail.com>
2025-05-20 14:54:33 +0530

    merge sort

---
+++ Sorts/merge_sort.py
@@ -0,0 +1,36 @@
+# Merge Sort
+# Time Complexity: O(n log n)
+# Space Complexity: O(n)
+
+def merge(arr, low, mid, high):
+    temp = []
+    left = low
+    right = mid+1
+    while left <= mid and right <= high:
+        if arr[left] <= arr[right]:
+            temp.append(arr[left])
+            left += 1
+        else:
+            temp.append(arr[right])
+            right += 1
+    while left <= mid:
+        temp.append(arr[left])
+        left += 1
+    while right <= high:
+        temp.append(arr[right])
+        right += 1
+    for i in range(low, high+1):
+        arr[i] = temp[i-low]
+
+
+def merge_sort(arr,low, high):
+    if low >=  high:
+        return
+    mid = (low+high)//2
+    merge_sort(arr, low, mid)
+    merge_sort(arr, mid+1, high)
+    merge(arr, low, mid, high)
+
+arr = [13, 46, 24, 52, 20, 9, 1]
+merge_sort(arr, 0, len(arr)-1)
+print(*arr) 
