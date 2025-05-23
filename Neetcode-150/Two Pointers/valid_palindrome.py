5908eb53fa09f985eee8130f25d5619832f71c9e
diya-kailash <diyakailash04@gmail.com>
2025-05-23 15:21:17 +0530

    valid palindrome

---
+++ Neetcode-150/Two Pointers/valid_palindrome.py
@@ -0,0 +1,28 @@
+# Valid Palindrome
+# Overall Time Complexity: O(n)
+# Overall Space Complexity: O(1)
+# Hint: Two-pointer technique 
+
+def isPalindrome(s):
+    i, j = 0, len(s) - 1
+    while i < j:
+        while i < j and not s[i].isalnum():
+            i += 1
+        while j > i and not s[j].isalnum():
+            j -= 1
+        if s[i].lower() != s[j].lower():  
+            return False
+        i += 1
+        j -= 1
+    return True
+
+# Time Complexity:
+# - Scans each character at most once: O(n)
+#   - Comparing valid characters: O(1)
+# Space Complexity:
+# - Uses only a few integer variables (i, j): O(1)
+
+s = "Was it a car or a cat I saw?"
+print(isPalindrome(s))  # Output: True
+s = "tab a cat"
+print(isPalindrome(s)) # Output: False
